4cbdf76c1dfe1123508e887bccec065bb852b6e1
Anchit Bajaj <ab@abifog.com>
2020-09-07 00:27:56 -0400

    Add Koji builds to dashboard

--- packit_dashboard/config.py
+++ packit_dashboard/config.py
@@ -2,4 +2,5 @@ from os import getenv
 
 deployment = getenv("DEPLOYMENT", "stg")
 
-API_URL = f"https://{deployment}.packit.dev/api"
+# API_URL = f"https://{deployment}.packit.dev/api"
+API_URL = f"https://packit.abifog.com:8443/api"
